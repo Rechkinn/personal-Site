5827ec8142c321e5990b787329a724e5278bef83
Aleksey Rechkinn <al.red197218@gmail.com>
2025-07-01 21:12:56 +0700

    01-07 21:12

--- js/moveWorkPhoto.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-    let workBodyPhoto = document.querySelector('.section__work-body-photo');
-    const sectionWork = document.querySelector('.section__work')
-    sectionWork.addEventListener("click", () => {
-        workBodyPhoto.classList.toggle('section__work-body-photo_transform-right');
-    });
-})
