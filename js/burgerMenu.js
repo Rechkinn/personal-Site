a77614dc8a5503e33b8958b785f3b6ef4fab59cc
Aleksey Rechkinn <al.red197218@gmail.com>
2025-07-03 14:27:42 +0700

    03-07 14:27

--- js/burgerMenu.js
+++ js/burgerMenu.js
@@ -3,9 +3,13 @@ function openCloseModalWindow() {
     let modalWindow = document.querySelector('.header__burger-modal-window');
     let menu = document.querySelector('.header__list');
     let switchCustom = document.querySelector('.switch');
+    let main = document.querySelector('.main');
+    main.classList.toggle('hidden');
+    main.classList.toggle('position-absolute');
     modalWindow.classList.toggle('hidden');
     menu.classList.toggle('show');
     switchCustom.classList.toggle('show');
+    
 
     let arrSpan = document.querySelectorAll('.burger-menu__span');
     arrSpan.forEach(span => {
